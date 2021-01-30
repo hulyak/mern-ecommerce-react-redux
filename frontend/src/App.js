8bd37b2c51564e37a0d363af951f6ffc3d06e14b
hulya <hulyakarakayaa06@gmail.com>
2021-01-29 16:50:59 -0800

    create the home screen products component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,18 @@
-import Footer from "./components/Footer";
-import Header from "./components/Header";
-import { Container } from 'react-bootstrap'
+import Footer from './components/Footer';
+import Header from './components/Header';
+import { Container } from 'react-bootstrap';
+import HomeScreen from './components/Screens/HomeScreen';
+
 function App() {
   return (
     <>
-     <Header />
-     <main className="py-3">
-     <Container>
-    <h1>Welcome</h1>
-     </Container>
-       
-     </main>
-     <Footer />
+      <Header />
+      <main className='py-3'>
+        <Container>
+         <HomeScreen />
+        </Container>
+      </main>
+      <Footer />
     </>
   );
 }
