9d17663e4fc47fe0f98722ea4c8078735e3168df
hulya <hulyakarakayaa06@gmail.com>
2021-01-29 16:14:01 -0800

    set up bootstrap and add font awesome icons

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,18 @@
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import { Container } from 'react-bootstrap'
 function App() {
   return (
-    <div>
-     Welcome
-    </div>
+    <>
+     <Header />
+     <main className="py-3">
+     <Container>
+    <h1>Welcome</h1>
+     </Container>
+       
+     </main>
+     <Footer />
+    </>
   );
 }
 
